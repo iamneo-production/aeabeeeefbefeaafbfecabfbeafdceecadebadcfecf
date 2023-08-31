b9746dfd7c9a27ac6ca1f62f1c76083011d47723
Iamneo <techadmin@iamneo.ai>
2023-08-31 09:10:30 +0000

    Backup Commit

--- angularapp/src/app/app.component.spec.ts
+++ angularapp/src/app/app.component.spec.ts
@@ -1,35 +1,52 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+beforeEach(async(() => {
+TestBed.configureTestingModule({
+declarations: [
+AppComponent
+],
+imports: [
+ReactiveFormsModule
+]
+}).compileComponents();
+}));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have as title 'angularapp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularapp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularapp app is running!');
-  });
+it('should create the app', () => {
+const fixture = TestBed.createComponent(AppComponent);
+const app = fixture.componentInstance;
+expect(app).toBeTruthy();
 });
+
+it('should have a form group', () => {
+const fixture = TestBed.createComponent(AppComponent);
+const app = fixture.componentInstance;
+expect(app.registrationForm).toBeTruthy();
+});
+
+it('should be invalid if fields are empty', () => {
+const fixture = TestBed.createComponent(AppComponent);
+const app = fixture.componentInstance;
+expect(app.registrationForm.valid).toBeFalsy();
+});
+it('should be invalid if age is required but not provided', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.componentInstance;
+  const countryControl = app.registrationForm.get('country');
+  const ageControl = app.registrationForm.get('age');
+  if (countryControl && ageControl) {
+    countryControl.setValue('US');
+    expect(app.registrationForm.valid).toBeFalsy();
+    ageControl.setValue('25');
+    expect(app.registrationForm.valid).toBeTruthy();
+  }
+}
+);
+});
+
+
+
+
